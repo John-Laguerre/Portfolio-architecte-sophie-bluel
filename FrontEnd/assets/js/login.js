a47900e00b931d3aa18905bc7705c49692a7f8e9
john laguerre <john.laguerre14@gmail.com>
2023-10-11 16:10:58 +0200

    Modification du fichier

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,95 +1,69 @@
-// Sélectionnez les éléments du DOM
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const submitButton = document.querySelector('#login-submit');
+// login.js
 
-// Gestionnaire d'événement pour la saisie dans le champ e-mail
-emailInput.addEventListener('input', () => {
-    // Efface les messages d'erreur précédents pour le champ e-mail
-    hideValidationError(emailInput, false);
+// Attend que le document HTML soit chargé avant d'exécuter le code
+document.addEventListener("DOMContentLoaded", function () {
+  // Récupère le formulaire de connexion et le message d'erreur par leur ID
+  const loginForm = document.getElementById("loginForm");
+  const errorMessage = document.getElementById("errorMessage");
 
-    // Vérifie si le champ e-mail est vide
-    if (emailInput.value.trim() === '') {
-        // Affiche un message d'erreur si le champ est vide
-        showValidationError(emailInput, false);
-    } else if (!validateEmail(emailInput.value.trim())) {
-        // Vérifie si le format de l'e-mail est incorrect et affiche un message d'erreur
-        showValidationError(emailInput, false, 'Format incorrect');
-    } else {
-        // Cache les messages d'erreur s'il n'y a pas d'erreur
-        hideValidationError(emailInput, false);
+  // Ajoute un gestionnaire d'événements lorsque le formulaire est soumis
+  loginForm.addEventListener("submit", async function (e) {
+    e.preventDefault(); // Empêche la soumission du formulaire par défaut
+
+    // Récupère les valeurs de l'email et du mot de passe depuis les champs du formulaire
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Crée un objet avec les informations d'identification de l'utilisateur
+    const userCredentials = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      // Effectue une requête asynchrone vers le serveur pour l'authentification
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userCredentials), // Convertit les informations en JSON
+      });
+
+      if (response.status === 200) {
+        // Si l'authentification est réussie (statut 200), traite la réponse
+        const data = await response.json(); // Récupère les données JSON de la réponse
+        window.localStorage.setItem("token", data.token); // Stocke le token dans le stockage local
+        window.location.href = 'index.html'; // Redirige vers la page d'accueil
+      } else {
+        // Si l'authentification échoue, affiche un message d'erreur
+        errorMessage.textContent =
+          "L'authentification a échoué. Vérifiez vos informations.";
+      }
+    } catch (error) {
+      console.error(error); // En cas d'erreur, affiche l'erreur dans la console
     }
+  });
+
+  // Récupère le lien de connexion dans la barre de navigation
+  const loginLink = document.querySelector("li#login-link");
+  // Récupère le nom de la page actuellement affichée dans l'URL
+  const currentPage = window.location.pathname.split("/").pop();
+
+  // Vérifie si la page actuelle est "login.html" et ajoute une classe "active" au lien
+  if (currentPage === "login.html") {
+    loginLink.classList.add("active");
+  }
 });
 
-// Gestionnaire d'événement pour la saisie dans le champ de mot de passe
-passwordInput.addEventListener('input', () => {
-    if (passwordInput.value === '') {
-        // Affiche un message d'erreur si le champ du mot de passe est vide
-        showValidationError(passwordInput, false);
-    } else {
-        // Cache les messages d'erreur s'il n'y a pas d'erreur
-        hideValidationError(passwordInput, false);
-    }
-});
+// Gestionnaire d'événements pour le bouton de déconnexion
+const logoutButton = document.getElementById("logoutButton");
 
-// Gestionnaire d'événement pour le clic sur le bouton de soumission du formulaire
-submitButton.addEventListener('click', async (e) => {
-    // Vérifie la validité du formulaire
-    checkLoginFormValidity(emailInput, passwordInput);
-
-    e.preventDefault();
-
-    // Vérifie que le champ d'e-mail et le champ du mot de passe n'ont pas de classes "error-input"
-    if (!emailInput.classList.contains("error-input") && !passwordInput.classList.contains("error-input")) {
-        // Récupère les valeurs du champ d'e-mail et du champ de mot de passe
-        const email = emailInput.value;
-        const password = passwordInput.value;
-
-        // Vérifie le format de l'e-mail
-        if (!validateEmail(email)) {
-            // Affiche un message d'erreur si le format de l'e-mail est incorrect
-            showValidationError(emailInput, false, 'Format incorrect');
-            return;
-        }
-
-        // Crée un objet avec les informations d'identification de l'utilisateur
-        const data = {
-            email: email,
-            password: password
-        };
-
-        try {
-            // Effectue une requête asynchrone vers le serveur pour l'authentification
-            const response = await fetch("http://localhost:5678/api/users/login", {
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            });
-
-            if (response.ok) {
-                // Si l'authentification est réussie, traite la réponse
-                const responseData = await response.json();
-                const token = responseData.token;
-
-                // Stocke le token dans le stockage local
-                localStorage.setItem("token", token);
-
-                // Redirige vers la page d'accueil
-                window.location.href = "index.html";
-            } else if (response.status === 401) {
-                // Si l'authentification échoue, affiche un message d'erreur
-                throw new Error("Adresse mail ou mot de passe invalide");
-            } else {
-                // En cas d'erreur de connexion, affiche un message d'erreur générique
-                throw new Error("Erreur lors de la connexion");
-            }
-        } catch (error) {
-            // En cas d'erreur, affiche l'erreur dans le champ de mot de passe
-            showValidationError(passwordInput, false, error.message);
-            passwordInput.value = '';
-        }
-    }
-});
+if (logoutButton) {
+  logoutButton.addEventListener("click", function () {
+    // Supprimez le token de l'utilisateur du stockage local
+    localStorage.removeItem("token");
+    // Redirigez l'utilisateur vers la page d'accueil
+    window.location.href = 'index.html';
+  });
+}
