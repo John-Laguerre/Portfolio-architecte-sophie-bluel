685f24e8592b0dfd7f75c13cfbfcafdfd34e9789
john laguerre <john.laguerre14@gmail.com>
2023-10-17 20:32:47 +0200

    appel des url

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,6 +1,11 @@
 // Sélectionner l'élément gallery dans le DOM
 const galleryElement = document.querySelector('.gallery');
 
+const urlWorks = "http://localhost:5678/api/works";
+const urlCategorie = "http://localhost:5678/api/categories";
+const urlDeleteWork = "http://localhost:5678/api/works/";
+
+
 
 
 
