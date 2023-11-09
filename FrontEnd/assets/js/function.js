224285306e9032f44e920ab061670d73a938946d
john laguerre <john.laguerre14@gmail.com>
2023-11-10 00:24:54 +0100

    modification

--- FrontEnd/assets/js/function.js
+++ FrontEnd/assets/js/function.js
@@ -1,3 +1,12 @@
+// Fonction pour modifier l'attribut aria-hidden des éléments
+function updateAriaHidden(elements, value) {
+  elements.forEach(element => {
+    if (element) {
+      element.setAttribute("aria-hidden", value);
+    }
+  });
+}
+
 // Fonction pour masquer les éléments
 function hideElements(elements) {
     elements.forEach(element => {
@@ -24,73 +33,82 @@ function toggleElements(elements, visible) {
       }
     });
 }
-  
+
+// Filter
+function filterProjects(category) {
+	const gallery = document.querySelector('.gallery');
+	const projects = gallery.querySelectorAll('figure');
+
+	projects.forEach(project => {
+		const projectCategory = project.dataset.category;
+
+		if (category === 'all' || projectCategory === category) {
+            const show = [project];
+			showElements(show);
+		} else {
+            const hide = [project];
+            hideElements(hide);
+		}
+	});
+}
+
 // Fonction pour supprimer un projet
 function deleteProject(projectId) {
-    const token = localStorage.getItem("token");
-    const elementDeleted = document.querySelector(`.delete-icon[data-project-id="${projectId}"]`);
-    const portfolioDeleted = document.querySelector(`figure[data-project-id="${projectId}"]`);
-    const galleryDeleted = elementDeleted.parentElement;
-  
-    fetch(`http://localhost:5678/api/works/${projectId}`, {
-      method: 'DELETE',
-      headers: {
-        'Authorization': `Bearer ${token}`
+  const token = localStorage.getItem("token");
+  const elementDeleted = document.querySelector(`.delete-icon[data-project-id="${projectId}"]`);
+  const portfolioDeleted = document.querySelector(`figure[data-project-id="${projectId}"]`);
+  const galleryDeleted = elementDeleted.parentElement;
+
+  fetchDelete(projectId, token)
+    .then(response => {
+      if (response.ok) {
+        portfolioDeleted.remove();
+        galleryDeleted.remove();
+        console.log(`Le projet avec l'ID ${projectId} a été supprimé.`);
+      } else {
+        console.log(`Une erreur s'est produite lors de la suppression du projet avec l'ID ${projectId}.`);
       }
     })
-      .then(response => {
-        if (response.ok) {
-          portfolioDeleted.remove();
-          galleryDeleted.remove();
-          console.log(`Le projet avec l'ID ${projectId} a été supprimé.`);
-        } else {
-          console.log(`Une erreur s'est produite lors de la suppression du projet avec l'ID ${projectId}.`);
-        }
-      })
-      .catch(error => {
-        console.log('Une erreur s\'est produite lors de la communication avec l\'API :', error);
-      });
+    .catch(error => {
+      console.log('Une erreur s\'est produite lors de la communication avec l\'API :', error);
+    });
 }
-  
-  const buttonAdd = document.querySelector('.button-add');
-  const addText = document.querySelector('.add-text');
-  const imgIcon = document.querySelector('.imgIcon');
-  const errorText = document.querySelector('.error');
-  
-  const elementsToHidePreview = [buttonAdd, addText, imgIcon, errorText];
-  const elementsToShowPreview = [previewImg];
-  const elementsToHideRemove = [previewImg];
-  const elementsToShowRemove = [buttonAdd, addText, imgIcon, errorText];
-  
-// Fonction pour prévisualiser une image
+
+// Preview Picture
 function previewPicture() {
-    hideElements(elementsToHidePreview);
-    showElements(elementsToShowPreview);
-  
-    const picture = fileInput.files[0];
-    previewImg.src = URL.createObjectURL(picture);
+  const elementsToHide = [buttonAdd, addText, imgIcon, errorText];
+  const elementsToShow = [previewImg];
+
+  hideElements(elementsToHide);
+  showElements(elementsToShow);
+
+  const picture = fileInput.files[0];
+  previewImg.src = URL.createObjectURL(picture);
 }
-  
-// Fonction pour supprimer la prévisualisation de l'image
+
+// Remove Preview
 function removePreviewPicture() {
-    showElements(elementsToShowRemove);
-    hideElements(elementsToHideRemove);
-  
-    errorText.textContent = "";
-  
-    previewImg.src = "";
+  const elementsToHide = [previewImg];
+  const elementsToShow = [buttonAdd, addText, imgIcon, errorText];
+
+  showElements(elementsToShow);
+  hideElements(elementsToHide);
+
+  errorText.textContent = "";
+
+  previewImg.src = "";
 }
-  
-// Fonction pour réinitialiser le formulaire
+
+// Reset Form
 function resetForm() {
-    addImgForm.reset();
-    removePreviewPicture();
-    hideValidationError(titleInput);
-    hideValidationError(categorySelect);
-    hideValidationError(fileInputDiv);
-    disableSubmit();
+  addImgForm.reset();
+  removePreviewPicture();
+  hideValidationError(titleInput);
+  hideValidationError(categorySelect);
+  hideValidationError(fileInputDiv);
+  disableSubmit();
 }
-  
+
 // Fonction pour afficher un message d'erreur dans le formulaire
 function showValidationError(inputElement, submit = true, text = 'Ce champ doit être rempli') {
     const errorElement = inputElement.parentNode.querySelector(`.error-message[data-input="${inputElement.id}"]`);
@@ -134,7 +152,23 @@ function disableSubmit() {
       submitButton.classList.add('disabled');
     }
 }
-  
+
+// Check Form Validity
+function checkFormValidity(elements) {
+
+  elements.forEach(el => {
+      if(el.value.trim() === '') {
+          if(el.id==='') {
+              el = el.parentElement.parentElement;
+          }
+          showValidationError(el);
+      } else {
+          hideValidationError(el);
+      }
+  });
+  disableSubmit();
+}
+
 // Fonction pour vérifier la validité du formulaire de connexion
 function checkLoginFormValidity(email, password) {
     if (email.value.trim() === '') {
