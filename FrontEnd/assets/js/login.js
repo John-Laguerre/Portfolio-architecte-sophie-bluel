49e14d5473fbfd43411d50057e378ff99c835fda
john laguerre <john.laguerre14@gmail.com>
2023-11-13 01:21:41 +0100

    Ajout de commentaire

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,29 +1,39 @@
+// Attend que le DOM soit entièrement chargé avant d'exécuter le code
 document.addEventListener('DOMContentLoaded', function () {
+  
+  // Récupère les éléments du formulaire
   const emailInput = document.getElementById('email');
   const passwordInput = document.getElementById('password');
   const submitButton = document.getElementById('login-submit');
 
+  // Ajoute un gestionnaire d'événements au clic sur le bouton de soumission
   submitButton.addEventListener('click', async (e) => {
     e.preventDefault();
-  
+
+    // Vérifie la validité du formulaire de connexion
     checkLoginFormValidity(emailInput, passwordInput);
-  
+
+    // Si le formulaire est valide
     if (!emailInput.classList.contains('error-input') && !passwordInput.classList.contains('error-input')) {
       const email = emailInput.value;
       const password = passwordInput.value;
-  
+
+      // Si l'adresse e-mail n'est pas au bon format, affiche une erreur
       if (!validateEmail(email)) {
         showValidationError(emailInput, 'Format incorrect');
         return;
       }
-  
+
+      // Prépare les données à envoyer pour la connexion
       const data = {
         email: email,
         password: password,
       };
-  
+
+      // Effectue la requête de connexion
       fetchLogin(data)
         .then(response => {
+          // Si la connexion réussit, traite la réponse
           if (response.ok) {
             return response.json();
           } else if (response.status === 401) {
@@ -33,46 +43,49 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         })
         .then(responseData => {
+          // Stocke le token dans le stockage local et redirige vers la page d'accueil
           const token = responseData.token;
           localStorage.setItem('token', token);
           window.location.href = 'index.html';
         })
         .catch(error => {
+          // Affiche une erreur en cas d'échec de la connexion
           showValidationError(passwordInput, error.message);
           passwordInput.value = '';
         });
     }
   });
 
- // Récupère le lien de connexion dans la barre de navigation
- const loginLink = document.querySelector(".login-link");
+  // Récupère le lien de connexion dans la barre de navigation
+  const loginLink = document.querySelector(".login-link");
 
- // Récupère le nom de la page actuellement affichée dans l'URL
- const currentPage = window.location.pathname.split("/").pop();
+  // Récupère le nom de la page actuellement affichée dans l'URL
+  const currentPage = window.location.pathname.split("/").pop();
 
- // Vérifie si la page actuelle est "login.html" et ajoute une classe "active" au lien
- if (currentPage === "login.html") {
-   loginLink.classList.add("active");
- }
+  // Vérifie si la page actuelle est "login.html" et ajoute une classe "active" au lien
+  if (currentPage === "login.html") {
+    loginLink.classList.add("active");
+  }
 
+  // Ajoute un gestionnaire d'événements à la saisie dans le champ d'e-mail
   emailInput.addEventListener('input', function () {
-  hideValidationError(emailInput, false);
-  if (emailInput.value.trim() === '') {
-      showValidationError(emailInput, false);
-  } else if (!validateEmail(emailInput.value.trim())) {
-      showValidationError(emailInput, false, 'Format incorrect');
-  } else {
-      hideValidationError(emailInput, false);
-  }
+    hideValidationError(emailInput, false);
+    if (emailInput.value.trim() === '') {
+        showValidationError(emailInput, false);
+    } else if (!validateEmail(emailInput.value.trim())) {
+        showValidationError(emailInput, false, 'Format incorrect');
+    } else {
+        hideValidationError(emailInput, false);
+    }
   });
 
+  // Ajoute un gestionnaire d'événements à la saisie dans le champ de mot de passe
   passwordInput.addEventListener('input', function () {
-  if (passwordInput.value === '') {
-      showValidationError(passwordInput, false);
-  } else {
-      hideValidationError(passwordInput, false);
-  }
+    if (passwordInput.value === '') {
+        showValidationError(passwordInput, false);
+    } else {
+        hideValidationError(passwordInput, false);
+    }
   });
- 
-});
 
+});
